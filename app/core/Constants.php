dba1f3b82323fbe2a8807627267c48a5a296e690
Alezcandra <alezcandra@MacBook-Pro-Alezcandra.local>
2022-10-10 15:19:44 +0700

    mengerjakan tugas phpmvc

---
+++ app/core/Constants.php
@@ -0,0 +1,4 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_A_193040064_phpmvc/public');
+?>
---
+++ app/models/User_model.php
@@ -0,0 +1,12 @@
+<?php 
+
+class User_model{
+    private $nama = 'Akbar';
+
+    public function getUser(){
+        return $this->nama;
+    }
+}
+
+
+?>
